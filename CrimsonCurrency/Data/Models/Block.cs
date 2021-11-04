1f196f9f52d2df6bb8f68891ccbcc6a1e72ea9ce
Canaan <reakak1@gmail.com>
2021-11-04 19:28:23 +0200

    added Block class with passing tests

---
+++ CrimsonCurrency/Data/Models/Block.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimsonCurrency.Data.Models
+{
+    public class Block
+    {
+        public DateTime Timestamp { get; set; }
+        public string LastHash { get; set; }
+        public string CurrentHash { get; set; }
+        public List<Block> Data { get; set; }
+
+
+        public Block(){}
+
+        public Block(DateTime timestamp, string lastHash, string hash, List<Block> data)
+        {
+            Timestamp = timestamp;
+            LastHash = lastHash ?? throw new ArgumentNullException(nameof(lastHash));
+            CurrentHash = hash ?? throw new ArgumentNullException(nameof(hash));
+            Data = data ?? new List<Block>();
+        }
+
+        // the first block to ever exist
+        public static Block Genesis() => new Block(DateTime.UtcNow, "------", "f1r57-h45h", new List<Block>());
+
+        //create a new block thru mining ?? 
+
+        public static Block MineBlock(Block lastBlock, List<Block> data)
+        {
+            var timestamp = DateTime.UtcNow;
+            var lastHash = lastBlock.CurrentHash;
+            var hash = Block.Hash(timestamp, lastHash, data);
+
+            return new Block(timestamp, lastHash, hash, data);
+        }
+
+
+        public static string Hash(DateTime timeStamp, string lasthash, List<Block> data)
+            => ComputeSha256Hash($"{timeStamp}{lasthash}{data}");
+
+
+        public static string HashBlock(Block block) 
+            => ComputeSha256Hash($"{block.Timestamp}{block.LastHash}{block.Data}");
+
+
+        private static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ CrimsonCurrencyTests/BlockTests.cs
@@ -0,0 +1,66 @@
+using CrimsonCurrency.Data.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+namespace CrimsonCurrencyTests
+{
+    [TestClass]
+    public class BlockTests
+    {
+        public Block block { get; set; }
+        public List<Block> Data { get; set; } = new List<Block> { Block.Genesis() };
+        public Block LastBlock { get; set; }  
+        public Block CurrentBlock { get; set; } 
+
+        [TestMethod]
+        public void AssertGenesisBlockSameAsFirstBlockInBlock()
+        {
+            block = new Block();
+            block.Data = new List<Block> {Block.Genesis() };
+            Assert.AreEqual(block.Data[0].CurrentHash, Block.Genesis().CurrentHash);
+        }
+
+        [TestMethod]
+        public void AssertGenesisNotEqualTameredGenesisInBlock()
+        {
+            block = new Block();
+            block.Data = new List<Block> { Block.Genesis() };
+            block.Data[0].CurrentHash = "";
+            Assert.AreNotEqual(block.Data[0].CurrentHash, Block.Genesis().CurrentHash);
+        }
+
+
+
+
+        [TestMethod]
+        public void AssertCreatedHashIsSameAsCurrentHashForBlock()
+        {
+            block = new Block(
+                System.DateTime.UtcNow,
+                Block.Genesis().CurrentHash,
+                Block.Hash(System.DateTime.UtcNow, Block.Genesis().CurrentHash,Data ),
+                Data
+                );
+            Data.Add(block);
+            var block2 = Block.MineBlock(block, Data);
+
+            Assert.AreEqual(block2.LastHash, block.CurrentHash);
+
+        }
+
+        [TestMethod]
+        public void AssertHashBlockEqualCurrenthashForSameBlock()
+        {
+            block = new Block(
+                System.DateTime.UtcNow,
+                Block.Genesis().CurrentHash,
+                Block.Hash(System.DateTime.UtcNow, Block.Genesis().CurrentHash, Data),
+                Data
+            );
+
+            Assert.AreEqual(Block.HashBlock(block), block.CurrentHash);
+        }
+    }
+}
